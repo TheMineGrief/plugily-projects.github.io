c1e1cb09e986b087f7722572e458df4c4bb65077
gh-actions <actions@gihub.com>
2021-03-11 17:54:30 +0000

    Deploy website - based on f64c2798aaba61e8fad00d1130048527f6b05b67

---
+++ assets/js/afc1b2c1.ec66f637.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[55],{127:function(p){p.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"plugily-projects","name":"plugily projects","count":1,"permalink":"/blog/tags/plugily-projects"}')}}]);
---
+++ assets/js/dea1e647.cee76de0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[69],{141:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Introducing our new Wiki","permalink":"/blog/new-wiki"}]}')}}]);
